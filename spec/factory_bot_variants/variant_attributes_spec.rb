45e50068f923b6f4877406c9729ff055fadc7e02
Oliver Peate <oliverp@gmail.com>
2018-09-27 22:42:51 +0100

    Use ActiveSupport inflector to singularize attribute names

--- spec/factory_bot_variants/variant_attributes_spec.rb
+++ spec/factory_bot_variants/variant_attributes_spec.rb
@@ -1,4 +1,5 @@
 require "factory_bot_variants/variant_attributes"
+require "factory_bot_variants/inflector"
 
 RSpec.describe FactoryBotVariants::VariantAttributes, "::map" do
   it "supports symbol keys" do
@@ -11,5 +12,23 @@ RSpec.describe FactoryBotVariants::VariantAttributes, "::map" do
     variants = described_class.map({ "ages" => [1, 10, 20] })
 
     expect(variants).to eq [{ age: 1 }, { age: 10 }, { age: 20 }]
+  end
+
+  it "support singular keys" do
+    expect(described_class.map(age: [1, 10, 20])).
+      to eq(described_class.map(ages: [1, 10, 20]))
+  end
+
+  describe "converting attribute names from plural to singular" do
+    it "uses the inflector" do
+      allow(FactoryBotVariants::Inflector)
+        .to receive(:singularize).and_return(:octopus)
+
+      variants = described_class.map({ octopi: ["Inky", "Bubbles"] })
+
+      expect(variants.first.keys).to contain_exactly :octopus
+      expect(FactoryBotVariants::Inflector)
+        .to have_received(:singularize).with("octopi")
+    end
   end
 end
